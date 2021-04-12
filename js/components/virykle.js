6fae266af81a262371ab47d9f004ea2e1497bef6
Rimantas <belauzas@yahoo.com>
2021-04-12 17:19:34 +0300

    Config

--- js/components/virykle.js
+++ js/components/virykle.js
@@ -1,20 +1,20 @@
-function virykle(selector, holeCount) {
+function virykle(selector, holeCount, switchConfig = []) {
     // input validation
 
     // logic
     const DOM = document.querySelector(selector);
+    let configProvided = switchConfig.length > 0;
 
     // efektyviausias variantas generuoti 2 pasikartojancius tekstus
-    // let holesHTML = '';
-    // let switchesHTML = '';
-    // for (let i = 0; i < holeCount; i++) {
-    //     holesHTML += '<div class="hole" data-heating="false"></div>';
-    //     switchesHTML += '<div class="switch" data-heating="false"></div>';
-    // }
-
-    // neefektyviausias, bet trumpiausias variantas generuoti 2 pasikartojancius tekstus
-    const holesHTML = '<div class="hole" data-heating="false"></div>'.repeat(holeCount);
-    const switchesHTML = '<div class="switch" data-heating="false"></div>'.repeat(holeCount);
+    let holesHTML = '';
+    let switchesHTML = '';
+    for (let i = 0; i < holeCount; i++) {
+        if (!configProvided) {
+            switchConfig.push(i);
+        }
+        holesHTML += '<div class="hole" data-heating="false"></div>';
+        switchesHTML += '<div class="switch" data-heating="false"></div>';
+    }
 
     DOM.innerHTML = `<div class="holes">${holesHTML}</div>
                     <div class="switches">${switchesHTML}</div>`;
@@ -25,7 +25,7 @@ function virykle(selector, holeCount) {
 
     for (let i = 0; i < holeCount; i++) {
         const switchDOM = switchesDOM[i];
-        const holeDOM = holesDOM[i];
+        const holeDOM = holesDOM[switchConfig[i]];
         switchDOM.addEventListener('click', () => {
             const currentPosition = switchDOM.dataset.heating;
             switchDOM.dataset.heating = currentPosition === 'false' ? 'true' : 'false';
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import { virykle } from './components/virykle.js';
 
 virykle('#virykle', 4);
+virykle('#virykle2', 4, [2, 0, 1, 3]);
+
+// ar jungiklis-0 ijungia skyle-3 (mano pasirinkimas kode)
+// ar jungiklis-3 ijungia skyle-0
